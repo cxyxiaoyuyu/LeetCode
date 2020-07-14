097dc53df56268d7c01b5e734321d2e06500e39d
cxyxiaoyuyu.com <923996381@qq.com>
2020-07-14 22:51:39 +0800

    优化代码

--- 动态规划/120_三角形最小路径和.js
+++ 动态规划/120_三角形最小路径和.js
@@ -28,3 +28,19 @@ function minimumTotal(triangle){
     }
     return Math.min(...f[n-1])
 }
+
+// 优化代码
+const minimumTotal = (triangle) => {
+    const dp = new Array(triangle[triangle.length - 1].length);
+    // base case 是最后一行
+    for (let i = 0; i < dp.length; i++) {
+      dp[i] = triangle[triangle.length - 1][i];
+    }
+    // 从倒数第二列开始迭代
+    for (let i = dp.length - 2; i >= 0; i--) {
+      for (let j = 0; j < triangle[i].length; j++) {
+        dp[j] = Math.min(dp[j], dp[j + 1]) + triangle[i][j];
+      }
+    }
+    return dp[0];
+}
