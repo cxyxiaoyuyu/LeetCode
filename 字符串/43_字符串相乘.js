7c3ac35c1239a64544594195bdc35ee0896cda24
cxyxiaoyuyu <923996381@qq.com>
2020-08-13 10:35:21 +0800

    添加大神的解法

--- 字符串/43_字符串相乘.js
+++ 字符串/43_字符串相乘.js
@@ -51,3 +51,28 @@ var multiply = function(num1, num2) {
  const Add2 = (a,b) => {
      return String(BigInt(a) + BigInt(b))
  }
+
+
+ // 大神的解法  还没研究明白
+ const multiply = (num1, num2) => {
+    const len1 = num1.length;
+    const len2 = num2.length;
+    const pos = new Array(len1 + len2).fill(0);
+  
+    for (let i = len1 - 1; i >= 0; i--) {
+      const n1 = +num1[i];
+      for (let j = len2 - 1; j >= 0; j--) {
+        const n2 = +num2[j];
+        const multi = n1 * n2;
+        const sum = pos[i + j + 1] + multi;
+  
+        pos[i + j + 1] = sum % 10;
+        pos[i + j] += sum / 10 | 0;
+      }
+    }
+    let res = pos.join('');
+    while (res.length && res[0] == '0') {
+      res = res.substring(1);
+    }
+    return res.length ? res : '0';
+};
