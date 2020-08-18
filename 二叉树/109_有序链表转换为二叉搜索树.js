9d5049a8ce96f8ae53e69d39abfb69e49a62828a
cxyxiaoyuyu <923996381@qq.com>
2020-08-18 17:02:48 +0800

    快慢指针找根节点

--- 二叉树/109_有序链表转换为二叉搜索树.js
+++ 二叉树/109_有序链表转换为二叉搜索树.js
@@ -64,3 +64,29 @@ const sortedListToBST = (head) => {
     }
     return buildBST(0, arr.length - 1);
 };
+
+
+// 2 快慢指针 
+// 寻找链表的中间点，有个小技巧：快、慢指针指向头结点，快指针一次走两步，慢指针一次走一步，
+// 当快指针走到尾节点时，慢指针正好走到链表的中间。然后断开成两个链表，分而治之。
+// 为了断开，我们需要一个指针记录慢指针的前一个节点，因为单向链表的节点没有前驱指针。
+const sortedListToBST = (head) => {
+    if (head == null) return null;
+    let slow = head;
+    let fast = head;
+    let preSlow      // 保存slow的前一个节点
+  
+    while (fast && fast.next) {
+      preSlow = slow;
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+    const root = new TreeNode(slow.val);
+    if (preSlow !== undefined) {    // 中点slow不是head，需要构建左子树
+      preSlow.next = null           // 切断preSlow和中点slow
+      root.left = sortedListToBST(head); 
+    }
+    root.right = sortedListToBST(slow.next)
+    return root;
+};
+  
