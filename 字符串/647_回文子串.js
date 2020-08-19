0c08d5a54b3cf16fe6ead19bac91b33e97468f87
cxyxiaoyuyu <923996381@qq.com>
2020-08-19 11:57:14 +0800

    添加动态规划

--- 字符串/647_回文子串.js
+++ 字符串/647_回文子串.js
@@ -34,3 +34,53 @@ var countSubstrings = function(s) {
     }
     return res
 }
+
+// 2 动态规划  这个遍历顺序有点意思啊
+const countSubstrings = (s) => {
+    let count = 0;
+    const len = s.length;
+  
+    const dp = new Array(len);
+    for (let i = 0; i < len; i++) {
+      dp[i] = new Array(len).fill(false);
+    }
+  
+    for (let j = 0; j < len; j++) {
+      for (let i = 0; i <= j; i++) {
+        if (i == j) { // 单个字符
+          dp[i][j] = true;
+          count++;
+        } else if (j - i == 1 && s[i] == s[j]) { // 两个相同的字符 
+          dp[i][j] = true;
+          count++;
+        } else if (j - i > 1 && s[i] == s[j] && dp[i + 1][j - 1]) { // 多于两个字符
+          dp[i][j] = true;
+          count++;
+        }
+      }
+    }
+    return count;
+  };
+  
+// 代码优化
+const countSubstrings = (s) => {
+    let count = 0;
+    const len = s.length;
+  
+    const dp = new Array(len);
+    for (let i = 0; i < len; i++) {
+      dp[i] = new Array(len).fill(false);
+    }
+  
+    for (let j = 0; j < len; j++) {
+      for (let i = 0; i <= j; i++) {
+        if (i == j) { 
+          dp[i][j] = true;
+        } else if (s[i] == s[j]) {
+          dp[i][j] = j - i == 1 || dp[i + 1][j - 1];
+        }
+        if (dp[i][j]) count++;
+      }
+    }
+    return count;
+};
