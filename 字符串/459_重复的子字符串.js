c00729881ea52e1eb7489ba00390c9b91c5b55dc
cxyxiaoyuyu <923996381@qq.com>
2020-08-24 09:33:06 +0800

    字符串匹配

--- 字符串/459_重复的子字符串.js
+++ 字符串/459_重复的子字符串.js
@@ -26,3 +26,8 @@ var repeatedSubstringPattern = function(s) {
     }
     return false
 }
+
+// 2 我们将两个 ss 连在一起，并移除第一个和最后一个字符。如果 ss 是该字符串的子串，那么 ss 就满足题目要求。
+var repeatedSubstringPattern = function(s) {
+    return (s + s).indexOf(s, 1) != s.length
+}
