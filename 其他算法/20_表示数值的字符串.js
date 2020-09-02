60940d0979e8fbcb561ab029eed778a91832e1fd
cxyxiaoyuyu <923996381@qq.com>
2020-09-02 10:47:46 +0800

    通过穷举的方式判断是否时数字

--- 其他算法/20_表示数值的字符串.js
+++ 其他算法/20_表示数值的字符串.js
@@ -5,7 +5,7 @@
 // Number 
 // Number对于不能转为数值的变量返回 NaN  
 // 特殊情况  Number('') 空字符串会返回0  所以特殊判断
-var isNumber = function(s) {
+var isNumber = function (s) {
   s = s.trim()
   if (s == '') return false
   return !isNaN(Number(s))
@@ -13,9 +13,40 @@ var isNumber = function(s) {
 
 
 // isNaN(Number(xxx)) 等价于 isNaN(xxx)
-var isNumber = function(s) {
+var isNumber = function (s) {
   s = s.trim()
   if (s == '') return false
   return !isNaN(s)
 }
 
+// 2 模拟 穷举
+var isNumber = function (s) {
+  let isNum = false, isDot = false, isSymbol = false, isE = false
+  let s1 = s.trim()
+  for (let v = 0; v < s1.length; v++) {
+    // 不等于空格的判断非常有必要 !!!
+    // 这是字符串与数值比较 字符串会转为数值 空字符串会被转换为0  
+    // 所以 ' ' >=0 && ' ' <=9  ==> true  也符合条件
+    if (0 <= s1[v] && s1[v] <= 9 && s1[v] != ' ') {   
+      isNum = true
+    } else if (s1[v] == 'e' || s1[v] == 'E') {
+      if (!isNum || isE) {
+        return false
+      }
+      isE = true
+      isNum = false
+    } else if (s1[v] == '.') {
+      if (isDot || isE) {
+        return false
+      }
+      isDot = true
+    } else if (s1[v] == '-' || s1[v] == '+') {
+      if (v != 0 && s1[v - 1] != 'e' && s1[v - 1] != 'E') {
+        return false
+      }
+    } else {
+      return false
+    }
+  }
+  return isNum
+}
