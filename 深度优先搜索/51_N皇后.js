2d0557443f1a8ac16ba3f910f9dea18b211e92c2
cxyxiaoyuyu <923996381@qq.com>
2020-09-03 14:25:36 +0800

    优化代码

--- 深度优先搜索/51_N皇后.js
+++ 深度优先搜索/51_N皇后.js
@@ -18,57 +18,112 @@
 // 解释: 4 皇后问题存在两个不同的解法。
 
 // dfs  回溯
-var solveNQueens = function(n) {
+var solveNQueens = function (n) {
   let res = []
-  const dfs = (array,row,col)=>{
-      if(row >= n){
-          arr = array.map(row=>{
-             return row.join('')
-          })
-         res.push(arr)
-          return
-      }
-      if(col >= n){
-          return
-      }
-      // 判断  上方 左斜 右斜 是否有皇后了
-      if(hasQueue(array,row,col)){
-          dfs(array,row,col+1)
-      }else{
-          array[row][col] = 'Q'
-          dfs(array,row+1,0)
-          array[row][col] = '.'
-          dfs(array,row,col+1)
-      }
+  const dfs = (array, row, col) => {
+    if (row >= n) {
+      arr = array.map(row => {
+        return row.join('')
+      })
+      res.push(arr)
+      return
+    }
+    if (col >= n) {
+      return
+    }
+    // 判断  上方 左斜 右斜 是否有皇后了
+    if (hasQueue(array, row, col)) {
+      dfs(array, row, col + 1)
+    } else {
+      array[row][col] = 'Q'
+      dfs(array, row + 1, 0)
+      array[row][col] = '.'
+      dfs(array, row, col + 1)
+    }
   }
-  let array = Array.from(Array(n),()=>Array(n).fill('.'))
-  dfs(array,0,0)
+  let array = Array.from(Array(n), () => Array(n).fill('.'))
+  dfs(array, 0, 0)
   return res
 }
 
-function hasQueue(array,row,col){
+function hasQueue(array, row, col) {
   let n = array.length
-  if(row === 0) return false
+  if (row === 0) return false
 
   // 判断正上方
-  for(let i=row-1;i>=0;i--){
-      if(array[i][col] === 'Q'){
-          return true
-      }
+  for (let i = row - 1; i >= 0; i--) {
+    if (array[i][col] === 'Q') {
+      return true
+    }
   }
 
   // 判断左斜上方 
-  for(let i=row-1,j=col-1;i>=0&&j>=0;i--,j--){
-      if(array[i][j] === 'Q'){
-          return true
-      }
+  for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+    if (array[i][j] === 'Q') {
+      return true
+    }
   }
 
   // 判断右斜上方
-  for(let i=row-1,j=col+1;i>=0&&j<n;i--,j++){
-      if(array[i][j] === 'Q'){
-          return true
-      }
+  for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+    if (array[i][j] === 'Q') {
+      return true
+    }
   }
   return false
+}
+
+
+// 优化代码 列的话可以用循环做
+var solveNQueens = function (n) {
+  let res = []
+  let array = Array.from(Array(n), () => Array(n).fill('.'))
+
+  const hasQueue = (row, col) => {
+    let n = array.length
+    if (row === 0) return false
+
+    // 判断正上方
+    for (let i = row - 1; i >= 0; i--) {
+      if (array[i][col] === 'Q') {
+        return true
+      }
+    }
+
+    // 判断左斜上方 
+    for (let i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+      if (array[i][j] === 'Q') {
+        return true
+      }
+    }
+
+    // 判断右斜上方
+    for (let i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+      if (array[i][j] === 'Q') {
+        return true
+      }
+    }
+    return false
+  }
+
+  const dfs = (array, row) => {
+    if (row >= n) {
+      arr = array.map(row => {
+        return row.join('')
+      })
+      res.push(arr)
+      return
+    }
+    for (let col = 0; col < n; col++) {
+      // 如果满足条件 上 左斜上 右斜上都没有皇后
+      if (!hasQueue(row, col)) {
+        array[row][col] = 'Q'
+        dfs(array, row + 1)
+        array[row][col] = '.'
+      }
+    }
+  }
+
+  dfs(array, 0)
+  return res
 }
