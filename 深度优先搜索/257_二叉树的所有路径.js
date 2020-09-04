f2dcff832e9d77741d71a0bd841543f5168a27d7
cxyxiaoyuyu <923996381@qq.com>
2020-09-04 09:45:01 +0800

    用字符串存储路径 无需回溯

--- 深度优先搜索/257_二叉树的所有路径.js
+++ 深度优先搜索/257_二叉树的所有路径.js
@@ -14,51 +14,72 @@
 
 
 // 1 用数组存储路径 dfs  回溯
-var binaryTreePaths = function(root) {
-  if(root === null) return []
+var binaryTreePaths = function (root) {
+  if (root === null) return []
   let res = []
-  const dfs = (path,root)=>{
-      if(root.left === null && root.right === null){   // 叶子节点
-          path.push(root.val)
-          res.push( path.join('->') )
-          return
-      }
+  const dfs = (path, root) => {
+    if (root.left === null && root.right === null) {   // 叶子节点
       path.push(root.val)
+      res.push(path.join('->'))
+      return
+    }
+    path.push(root.val)
 
-      if(root.left){
-          dfs(path,root.left)
-          path.pop()
-      }
-      if(root.right){
-          dfs(path,root.right)
-          path.pop()
-      }
+    if (root.left) {
+      dfs(path, root.left)
+      path.pop()
+    }
+    if (root.right) {
+      dfs(path, root.right)
+      path.pop()
+    }
   }
-  dfs([],root)
+  dfs([], root)
   return res
 }
 
 // 2 不回溯也可以用临时变量存储当前层级的路径  不过要是换成 树的话,多个子节点就不适用了
-var binaryTreePaths = function(root) {
-  if(root === null) return []
+var binaryTreePaths = function (root) {
+  if (root === null) return []
   let res = []
-  const dfs = (path,root)=>{
-      if(root.left === null && root.right === null){   // 叶子节点
-          path.push(root.val)
-          res.push( path.join('->') )
-          return
-      }
+  const dfs = (path, root) => {
+    if (root.left === null && root.right === null) {   // 叶子节点
       path.push(root.val)
+      res.push(path.join('->'))
+      return
+    }
+    path.push(root.val)
 
-      let temp = path.slice()
-      if(root.left){
-          dfs(path,root.left)
-      }
-      if(root.right){
-          dfs(temp,root.right)
-      }
+    let temp = path.slice()
+    if (root.left) {
+      dfs(path, root.left)
+    }
+    if (root.right) {
+      dfs(temp, root.right)
+    }
   }
-  dfs([],root)
+  dfs([], root)
   return res
 }
 
+// 3 数组是引用类型 这题可以直接用字符串 就不用回溯的
+var binaryTreePaths = function (root) {
+  if (root === null) return []
+  let res = []
+  const dfs = (path, root) => {
+    if (root.left === null && root.right === null) {   // 叶子节点
+      res.push(path + root.val)
+      return
+    }
+    path += root.val + '->'
+
+    if (root.left) {
+      dfs(path, root.left)
+    }
+    if (root.right) {
+      dfs(path, root.right)
+    }
+  }
+  dfs('', root)
+  return res
+}
