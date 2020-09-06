c0f7a74f4c5ebfa07ba49488d696cef0d782a769
cxyxiaoyuyu.com <923996381@qq.com>
2020-09-06 13:37:35 +0800

    bfs判断二叉树是否是对称二叉树

--- 二叉树/101_对称二叉树.js
+++ 二叉树/101_对称二叉树.js
@@ -34,3 +34,49 @@ var isSymmetric = function(root) {
   } 
   return equalTree(root.left,root.right)
 };
+
+// 进阶：你可以运用递归和迭代两种方法解决这个问题吗？
+
+// 2 用迭代的方法  利用判断回文串的方式判断是不是对称
+var isSymmetric = function(root) {
+  let queue = [root]
+  while(queue.length){
+      let path = []
+      let n = queue.length
+      for(let i=0;i<n;i++){
+          node = queue.shift()
+          if(node === null){
+              path.push(null)
+          }else{
+              path.push(node.val)
+              if(node.left){
+                  queue.push(node.left)
+              }else{
+                  queue.push(null)
+              }
+              if(node.right){
+                  queue.push(node.right)
+              }else{
+                  queue.push(null)
+              }
+          }
+         
+      }
+      if(!isValid(path)){
+          return false
+      }
+  }
+  return true
+};
+
+function isValid(s){
+  let l = 0,r = s.length - 1
+  while(l < r){
+      if(s[l] !== s[r]){
+          return false
+      }
+      l++
+      r--
+  }
+  return true
+}
