697ee882d98e5e2ed10a1a66a9936af5ff019e42
cxyxiaoyuyu <923996381@qq.com>
2020-09-11 16:33:37 +0800

    堆排序求K

--- 哈希表/347_前K个高频元素.js
+++ 哈希表/347_前K个高频元素.js
@@ -30,3 +30,40 @@ var topKFrequent = function(nums, k) {
   })
   return res.slice(0,k)
 };
+
+// 最小堆
+var topKFrequent = function(nums, k) {
+  let map = {}
+  for(let i=0;i<nums.length;i++){
+      map[nums[i]] = map[nums[i]] || 0
+      map[nums[i]]++
+  }
+
+  let arr = Object.keys(map)
+  // 堆化前k个元素 即arr
+  for(let i=(k-1)>>1;i>=0;i--){      // 取根节点时取整 取整 
+    siftDown(i)  // 下沉这个元素
+  }
+
+  // 将剩下的元素依次与小根堆中的最小值进行比较
+  for(let i=k;i<arr.length;i++){
+    if(map[arr[i]] >= map[arr[0]]){
+      [arr[0],arr[i]] = [arr[i],arr[0]]
+      siftDown(0)
+    }
+  }
+
+  function siftDown(i){
+    const left = 2*i + 1, right = 2*i + 2
+    let greater = left
+    if(greater >= k) return  // 如果左节点超过k了
+    if(right < k && map[arr[right]] < map[arr[greater]]){
+      greater = right
+    }
+    if(map[arr[greater]] < map[arr[i]]){
+      [arr[greater],arr[i]] = [arr[i],arr[greater]]
+      siftDown(greater)
+    }
+  }
+  return arr.slice(0,k)
+}
